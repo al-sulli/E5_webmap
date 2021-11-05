87d1caa21a40c972214091e7723d0b12ed04021b
al-sulli <74216920+al-sulli@users.noreply.github.com>
2021-11-05 15:44:44 -0400

    commit

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ if __name__ == '__main__':
     dir(folium)
     map = folium.Map(location=(35.61, -82.44))
     map.save('data/map1.html')
+
+    import csv
+
+    with open("data/volcanoes.tsv") as fd:
+        rd = csv.reader(fd, delimiter="\t", quotechar='"')
+        for row in rd:
+            print(row)
